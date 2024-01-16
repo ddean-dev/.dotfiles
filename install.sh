e242446a10114c87b941cbf11cca967a528b2676
David Dean <david@ddean.dev>
2024-01-16 17:36:20 -0600

    Add nvim setup

--- install.sh
+++ install.sh
@@ -1,79 +1,38 @@
 #!/usr/bin/env bash
 
-VERSION_LUA="5.3.5"
-VERSION_LUAROCKS="3.9.2"
+# Install packages from apt
+sudo apt update && sudo apt upgrade
+sudo apt install -y zsh exa git curl screen tmux zip unzip golang fuse libfuse2 build-essential
 
-#install packages
-#sudo apt update && sudo apt upgrade
-#sudo apt install -y zsh exa git curl screen tmux zip unzip golang fuse libfuse2
-
-# Install starship
-if ! [ -x "$(command -v starship)" ]; then
-  echo "downloading starship"
-  curl -sS https://starship.rs/install.sh | sh
+# Install homebrew
+if [ ! -x "$(command -v brew)"]; then
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-# Install nvim
-if ! [ -x "$(command -v nvim)" ]; then
-  echo "downloading nvim"
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-  chmod +x nvim.appimage
-  sudo mv nvim.appimage /usr/local/bin/nvim
+# Install packages from brew
+brew update && brew upgrade
+brew install starship neovim lazygit node pnpm luarocks
+
+# Symlink config files
+ln -svf "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
+ln -svf "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -svf "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
+
+# Ensure zsh is shell
+echo "Setting Shell to ZSH"
+chsh -s $(which zsh)
+
+# Setup ssh key
+if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
+	echo "Generating SSH Key"
+	ssh-keygen -t ed25519 -C "david@ddean.dev"
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_ed25519
 fi
 
-# Install lazygit
-if ! [ -x "$(command -v lazygit)" ]; then
-  echo "downloading lazygit"
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  tar xf lazygit.tar.gz lazygit
-  sudo install lazygit /usr/local/bin
-fi
-
-# Install pnpm
-if ! [ -x "$(command -v pnpm)" ]; then
-  echo "downloading pnpm"
-  curl -fsSL https://get.pnpm.io/install.sh | sh -
-fi
-
-# Install lua
-if ! [ -x "$(command -v lua)" ]; then
-  curl -Lo "lua.tar.gz" "http://www.lua.org/ftp/lua-$VERSION_LUA.tar.gz"
-  tar -zxf "lua.tar.gz"
-  cd lua
-  sudo make install
-  cd ..
-  rm -r lua
-  rm lua.tar.gz
-
-  curl -Lo "luarocks.tar.gz" "https://luarocks.org/releases/luarocks-$VERSION_LUA.tar.gz"
-  tar -zxpf luarocks.tar.gz
-  cd luarocks
-  ./configure && make && sudo make install
-  cd ..
-  rm -r luarocks
-  rm luarocks.tar.gz
-fi
-
-#update config folder
-#if [ -e "$HOME/.dotfiles" ]; then
-#  echo "pulling dotfiles"
-#  git -C "$HOME/.dotfiles" pull
-#else
-#  echo "cloning dotfiles"
-#  git clone "https://github.com/ddean-dev/.dotfiles.git" "$HOME/.dotfiles/"
-#fi
-
-#symlink config files
-#ln -svf "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
-#ln -svf "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
-#ln -svf "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
-
-#ensure zsh is shell
-#echo "Setting Shell to ZSH"
-#chsh -s $(which zsh)
-
-#setup ssh key
+echo "Add below key to GitHub"
+cat ~/.ssh/id_ed25519.pub
+echo "Add above key to GitHub"
 
 #setup signing key
-
