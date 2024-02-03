df8052f01b3bda6912b3412e64cc307dfdb339fb
David Dean <david@ddean.dev>
2024-02-03 12:25:18 -0600

    remove unused paramaters

--- nvim/lua/plugins/folding.lua
+++ nvim/lua/plugins/folding.lua
@@ -23,7 +23,7 @@ return {
       vim.keymap.set("n", "zM", require('ufo').closeAllFolds, { desc = "Close all folds" })
       vim.keymap.set("n", "zR", require('ufo').openAllFolds, { desc = "Open all folds" })
       require('ufo').setup({
-        provider_selector = function(bufnr, filetype, buftype)
+        provider_selector = function()
           return { 'treesitter', 'indent' }
         end
       })
