e9de1c35bab05af53e89087f0665faf5b05d32fc
David Dean <david@ddean.dev>
2024-02-05 06:05:08 -0600

    set undo's default action

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -46,8 +46,24 @@ return {
     keys = {
       { "<leader>u", "<cmd>Telescope undo<cr>", desc = "Undo History" },
     },
-    config = function(_, opts)
-      require("telescope").setup(opts)
+    config = function()
+      require("telescope").setup(
+        {
+          extensions = {
+            undo = {
+              mappings = {
+                i = {
+                  ["<cr>"] = require("telescope-undo.actions").restore,
+                  ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                  ["<C-cr>"] = require("telescope-undo.actions").restore,
+                  ["<C-y>"] = require("telescope-undo.actions").yank_deletions,
+                  ["<C-r>"] = require("telescope-undo.actions").restore,
+                },
+              },
+            },
+          },
+
+        })
       require("telescope").load_extension("undo")
     end,
   },
