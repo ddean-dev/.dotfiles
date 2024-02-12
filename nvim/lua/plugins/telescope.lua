12a2138a6012464e9d5f880b7919aeadeedc10c6
David Dean <david@ddean.dev>
2024-02-12 06:06:25 -0600

    add additional telescope hotkeys

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,26 +5,32 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     lazy = false,
     keys = {
-      { "<leader>t",       "<cmd>Telescope<cr>",                           desc = "Telescope" },
-      { "<leader><space>", "<cmd>Telescope find_files<cr>",                desc = "Find File" },
-      { "<leader>/",       "<cmd>Telescope live_grep<cr>",                 desc = "Live Grep" },
-      { "<leader>.",       "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy Find" },
-      { "<leader>b",       "<cmd>Telescope buffers<cr>",                   desc = "Buffers" },
-      { "'",               "<cmd>Telescope marks<cr>",                     desc = "Marks" },
-      { "<leader>h",       "<cmd>Telescope help_tags<cr>",                 desc = "Help Search" },
-      { "<leader>d",       "<cmd>Telescope diagnostics<cr>",               desc = "Diagnostics" },
-      { "<leader>gh",      "<cmd>Telescope git_bcommits<cr>",              desc = "History" },
-      { "<leader>gc",      "<cmd>Telescope git_commits<cr>",               desc = "Commits" },
-      { "<leader>gb",      "<cmd>Telescope git_branches<cr>",              desc = "Branches" },
-      { "<leader>gS",      "<cmd>Telescope git_stash<cr>",                 desc = "Stash" },
-      { "<leader>gs",      "<cmd>Telescope git_status<cr>",                desc = "Status" },
-      { "gD",              "<cmd>Telescope lsp_declarations<cr>",          desc = "Go to Declaration" },
-      { "gd",              "<cmd>Telescope lsp_definitions<cr>",           desc = "Go to Definition" },
-      { "gi",              "<cmd>Telescope lsp_implementaions<cr>",        desc = "Go to Implementation" },
-      { "gr",              "<cmd>Telescope lsp_references<cr>",            desc = "Go to References" },
+      { "<leader>t", "<cmd>Telescope<cr>", desc = "Telescope" },
+      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find File" },
+      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+      { "<leader>.", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy Find" },
+      { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "'", "<cmd>Telescope marks<cr>", desc = "Marks" },
+      { "<leader>h", "<cmd>Telescope help_tags<cr>", desc = "Help Search" },
+      { "<leader>m", "<cmd>Telescope man_pages<cr>", desc = "Manual Pages" },
+      { "<leader>d", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+      { "<leader>gh", "<cmd>Telescope git_bcommits<cr>", desc = "History" },
+      { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
+      { "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Branches" },
+      { "<leader>gS", "<cmd>Telescope git_stash<cr>", desc = "Stash" },
+      { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Status" },
+      { "gD", "<cmd>Telescope lsp_declarations<cr>", desc = "Go to Declaration" },
+      { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Go to Definition" },
+      { "gi", "<cmd>Telescope lsp_implementaions<cr>", desc = "Go to Implementation" },
+      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "Go to References" },
+      { "<leader>s", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Symbols" },
+      { "<leader>:", "<cmd>Telescope symbols<cr>", desc = "Emoji🙂" },
     },
     opts = {
       pickers = {
+        symbols = {
+          sources = { 'emoji' },
+        },
         find_files = {
           find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
         }
@@ -66,5 +72,9 @@ return {
         })
       require("telescope").load_extension("undo")
     end,
+  },
+  {
+    "nvim-telescope/telescope-symbols.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
   },
 }
