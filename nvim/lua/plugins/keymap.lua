5eb15516b941dd3cb471bbf56cb81c45e78e693a
David Dean <david@ddean.dev>
2024-02-12 06:20:06 -0600

    add tmux stile pane splits

--- nvim/lua/plugins/keymap.lua
+++ nvim/lua/plugins/keymap.lua
@@ -6,5 +6,7 @@ vim.keymap.set("v", "<", "<gv", { desc = "Decrease indent" })
 vim.keymap.set("v", ">", ">gv", { desc = "Increase indent" })
 vim.keymap.set("n", "<C-Right>", "w", { desc = "Right word" })
 vim.keymap.set("n", "<C-Left>", "b", { desc = "Left word" })
+vim.keymap.set("n", "<C-w>%", "<cmd>vsplit<cr>", { desc = "Split Right" })
+vim.keymap.set("n", '<C-w>"', "<cmd>split<cr>", { desc = "Split Below" })
 
 return {}
