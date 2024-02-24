8c55a7e63f44758615af1ba86ae523b6c7e00ec1
David Dean <david@ddean.dev>
2024-02-24 02:15:03 -0600

    bind M-s to save without formating

--- nvim/lua/plugins/formater.lua
+++ nvim/lua/plugins/formater.lua
@@ -1,9 +1,14 @@
 return {
   'stevearc/conform.nvim',
-  opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  },
+  config = function()
+    vim.keymap.set({ "i", "x", "n", "s" }, "<M-s>", "<cmd>up<cr><esc>", { desc = "Save file" })
+    vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", function()
+      vim.cmd.stopinsert()
+      require("conform").format({
+        lsp_fallback = true,
+        timeout_ms = 500,
+      })
+      vim.cmd.update()
+    end, { desc = "Save file" })
+  end
 }
--- nvim/lua/plugins/keymap.lua
+++ nvim/lua/plugins/keymap.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
-vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>up<cr><esc>", { desc = "Save file" })
 vim.keymap.set("v", "<", "<gv", { desc = "Decrease indent" })
 vim.keymap.set("v", ">", ">gv", { desc = "Increase indent" })
 vim.keymap.set("n", "<C-Right>", "w", { desc = "Right word" })
