096e4af691470e9b0230b2c89c61cef640b71402
David Dean <david@ddean.dev>
2024-02-24 14:01:23 -0600

    add installation command to readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Dotfiles
 
-My debian config files and setup scripts.
+My terminal config files and setup scripts. Currently being used on Debian in WSL2 and SteamOS on steamdeck
+
+## Installation
+```sh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ddean-dev/.dotfiles/main/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-# Install homebrew
-if [ ! -x "$(command -v brew)" ]; then
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# Clone .dotfiles repo
+if [ ! -d "$HOME/.dotfiles/" ]; then
+  git clone https://github.com/ddean-dev/.dotfiles "$HOME/.dotfiles/"
 fi
+git -C "$HOME/.dotfiles/" fetch
+git -C "$HOME/.dotfiles/" pull
 
+# List of packages to install
 PACKAGES=""
 PACKAGES="$PACKAGES starship zsh-syntax-highlighting keychain"
 PACKAGES="$PACKAGES neovim lazygit"
 PACKAGES="$PACKAGES eza bat ripgrep fzf duf bottom"
 PACKAGES="$PACKAGES node pnpm luarocks go zig rust cmake"
 
+# TODO: install via native package managers
+
+# Install via hoebrew
+if [ ! -x "$(command -v brew)" ]; then
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
 brew install $PACKAGES
 
 # Intall tmux package manager
@@ -36,6 +45,11 @@ if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
   eval `keychain --eval --quiet --agents ssh`
   ssh-add ~/.ssh/id_ed25519
 fi
-echo "Add below key to GitHub"
+echo ""
+echo "###########################"
+echo "# Add below key to GitHub #"
+echo "###########################"
 cat ~/.ssh/id_ed25519.pub
-echo "Add above key to GitHub"
+echo "###########################"
+echo "# Add above key to GitHub #"
+echo "###########################"
