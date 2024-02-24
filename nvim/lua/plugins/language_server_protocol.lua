eb4f5c1ffa414ae35226a1163546481952a0bdf1
David Dean <david@ddean.dev>
2024-02-24 02:45:53 -0600

    rework some lsp keybinds

--- nvim/lua/plugins/language_server_protocol.lua
+++ nvim/lua/plugins/language_server_protocol.lua
@@ -25,23 +25,27 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = { "folke/neodev.nvim" },
-    keys = {
-      { 'K',         vim.lsp.buf.hover,          desc = "Hover" },
-      { '<C-K>',     vim.lsp.buf.signature_help, desc = "Signature Help", mode = { "n", "i" } },
-      { '<C-Enter>', vim.lsp.buf.code_action,    desc = "Code Action",    mode = { "n", "i" } },
-      { '<leader>r', vim.lsp.buf.rename,         desc = "Rename" },
-    },
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function()
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+          vim.keymap.set({ "n", "i" }, '<C-k>', vim.lsp.buf.signature_help, { desc = "Signature Help" })
+          vim.keymap.set("n", '<leader>q', vim.lsp.buf.code_action, { desc = "Code Action" })
+          vim.keymap.set("n", '<leader>r', vim.lsp.buf.rename, { desc = "Rename" })
+        end
+      })
+    end
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       local lsp = require("mason-lspconfig")
       lsp.setup()
-      require("mason-lspconfig").setup_handlers {
+      require("mason-lspconfig").setup_handlers({
         function(server_name)
           require("lspconfig")[server_name].setup({})
         end,
-      }
+      })
     end,
   },
 }
