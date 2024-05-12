2032e8208d5cdd0a1bc829860ea556191ac99de6
David Dean <david@ddean.dev>
2024-05-12 10:39:21 -0600

    setup formating properly

--- nvim/lua/plugins/formater.lua
+++ nvim/lua/plugins/formater.lua
@@ -1,14 +1,36 @@
 return {
-  'stevearc/conform.nvim',
-  config = function()
-    vim.keymap.set({ "i", "x", "n", "s" }, "<M-s>", "<cmd>up<cr><esc>", { desc = "Save file" })
-    vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", function()
-      vim.cmd.stopinsert()
-      require("conform").format({
-        lsp_fallback = true,
-        timeout_ms = 500,
-      })
-      vim.cmd.wall()
-    end, { desc = "Save file" })
-  end
+	"williamboman/mason.nvim",
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					html = { "prettier" },
+					css = { "prettier" },
+					json = { "prettier" },
+					go = { "goimports", "gofmt" },
+					shell = { "shfmt" },
+					proto = { "buf" },
+				},
+			})
+			vim.keymap.set({ "i", "x", "n", "s" }, "<M-s>", "<cmd>up<cr><esc>", { desc = "Save file" })
+			vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", function()
+				vim.cmd.stopinsert()
+				require("conform").format({
+					lsp_fallback = true,
+					timeout_ms = 500,
+				})
+				vim.cmd.up()
+			end, { desc = "Save file" })
+		end,
+	},
+	{
+		"zapling/mason-conform.nvim",
+		config = function()
+			require("mason-conform").setup()
+		end,
+	},
 }
