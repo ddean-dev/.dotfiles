ee74f0ac97ac221f97f21ddd6b5c79ac61cf894e
David Dean <david@ddean.dev>
2024-06-19 10:48:10 -0600

    fix eslint

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "mfussenegger/nvim-lint",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      vim.api.nvim_create_autocmd({ "TextChanged" }, {
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
-    end
-  },
-  {
-    --TODO: Return to main branch if https://github.com/rshkarin/mason-nvim-lint/pull/2 merged
-    --"rshkarin/mason-nvim-lint",
-    "ddean-dev/mason-nvim-lint",
-    branch = "quiet-mode",
-    dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-lint" },
-    config = function()
-      require("mason-nvim-lint").setup({
-        ensure_installed = {},
-        automatic_installation = true,
-        quiet_mode = true,
-      })
-    end
-  },
+	{
+		"mfussenegger/nvim-lint",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("lint").linters_by_ft = {
+				typescript = { "eslint" },
+			}
+			vim.api.nvim_create_autocmd({ "TextChanged", "BufWritePost", "BufEnter" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
+	{
+		"rshkarin/mason-nvim-lint",
+		dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-lint" },
+		config = function()
+			require("mason-nvim-lint").setup({
+				ensure_installed = {},
+				automatic_installation = true,
+				quiet_mode = true,
+			})
+		end,
+	},
 }
