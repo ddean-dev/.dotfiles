0149e90b6a934224e383fa42b2d612bb8faa4732
David Dean <david@ddean.dev>
2024-07-23 06:19:28 -0600

    formating

--- nvim/lua/plugins/folding.lua
+++ nvim/lua/plugins/folding.lua
@@ -1,26 +1,26 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("ibl").setup({
-        indent = {
-          char = "",
-          tab_char = "",
-        },
-      })
-    end,
-  },
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
-    config = function()
-      vim.keymap.set("n", "zM", require('ufo').closeAllFolds, { desc = "Close all folds" })
-      vim.keymap.set("n", "zR", require('ufo').openAllFolds, { desc = "Open all folds" })
-      require('ufo').setup({
-        provider_selector = function()
-          return { 'treesitter', 'indent' }
-        end
-      })
-    end,
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("ibl").setup({
+				indent = {
+					char = "",
+					tab_char = "",
+				},
+			})
+		end,
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+		config = function()
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+			require("ufo").setup({
+				provider_selector = function()
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	},
 }
