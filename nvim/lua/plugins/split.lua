d5d0629a833b70490c048fe53fe3690bda19f1fc
David Dean <david@ddean.dev>
2024-07-23 06:19:01 -0600

    convert auto command to lua

--- nvim/lua/plugins/split.lua
+++ nvim/lua/plugins/split.lua
@@ -1,20 +1,31 @@
-function _G.NewHelpSplit()
-	if vim.bo.buftype == "help" or vim.bo.filetype == "man" then
-		local origin_win = vim.fn.win_getid(vim.fn.winnr("#"))
-		local help_win = vim.api.nvim_get_current_win()
-		local help_buf = vim.fn.bufnr()
-		vim.api.nvim_set_current_win(origin_win)
-		vim.api.nvim_win_close(help_win, false)
-		vim.cmd("vsplit")
-		vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), help_buf)
-	end
-end
+local group = vim.api.nvim_create_augroup("HelpSplit", { clear = true })
+vim.api.nvim_create_autocmd("WinNew", {
+	group = group,
+	callback = function()
+		vim.api.nvim_create_autocmd("BufEnter", {
+			group = group,
+			once = true,
+			callback = function()
+				if vim.bo.buftype ~= "help" and vim.bo.filetype ~= "man" then
+					return
+				end
 
-vim.cmd([[
-  augroup HelpSplit
-    autocmd!
-    autocmd WinNew * autocmd BufEnter * ++once lua _G.NewHelpSplit()
-  augroup end
-]])
+				local origin_win = vim.fn.win_getid(vim.fn.winnr("#"))
+				local help_win = vim.api.nvim_get_current_win()
+				local help_buf = vim.fn.bufnr()
+				local width = vim.fn.winwidth(origin_win)
+
+				if width <= 90 then
+					return
+				end
+
+				vim.api.nvim_set_current_win(origin_win)
+				vim.api.nvim_win_close(help_win, false)
+				vim.cmd("vsplit")
+				vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), help_buf)
+			end,
+		})
+	end,
+})
 
 return {}
