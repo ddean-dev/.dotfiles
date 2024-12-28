7b81284037296f0134c811e08f539a7cf2ce1fa5
David Dean <david@ddean.dev>
2024-12-28 12:21:15 -0600

    change git signs to sign column instead of numhl

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,8 +3,8 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup({
-				signcolumn = false,
-				numhl = true,
+				signcolumn = true,
+				numhl = false,
 				word_diff = false,
 				current_line_blame = true,
 				current_line_blame_opts = {
@@ -17,7 +17,8 @@ return {
 					vim.keymap.set("n", "]g", "<cmd>Gitsigns next_hunk<CR>", { desc = "Next git change" })
 					vim.keymap.set("n", "[g", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Prev git change" })
 					vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>", { desc = "Undo Changes" })
-					vim.keymap.set({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Git changes" })
+					vim.keymap.set({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
+					vim.keymap.set({ "o", "x" }, "ag", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
 				end,
 				diff_opts = {
 					ignore_whitespace = true,
