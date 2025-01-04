32f2e4ebf9b2854bcda01d21f80eb88160c84a4d
David Dean <david@ddean.dev>
2025-01-04 12:10:48 -0600

    improve <C-v> binding on systems without their own handler

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -1,23 +1,41 @@
-local function clipboard()
-  require("telescope").extensions.yank_history.yank_history({})
-end
-
 return {
-  {
-    "gbprod/yanky.nvim",
-    dependencies = {
-      { "kkharji/sqlite.lua" }
-    },
-    opts = {
-      ring = { storage = "sqlite" },
-    },
-    keys = {
-      { "<C-v>", clipboard,                    desc = "Clipboard" },
-      { "y",     "<Plug>(YankyYank)",          desc = "Yank text",                                 mode = { "n", "x" } },
-      { "p",     "<Plug>(YankyPutAfter)",      desc = "Put yanked text after cursor",              mode = { "n", "x" } },
-      { "P",     "<Plug>(YankyPutBefore)",     desc = "Put yanked text before cursor",             mode = { "n", "x" } },
-      { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
-      { "<c-n>", "<Plug>(YankyNextEntry)",     desc = "Select next entry through yank history" },
-    },
-  }
+	{
+		"gbprod/yanky.nvim",
+		dependencies = {
+			{ "kkharji/sqlite.lua" },
+		},
+		opts = {
+			ring = { storage = "sqlite" },
+		},
+		keys = {
+			{
+				"<C-v>",
+				function()
+					require("telescope").extensions.yank_history.yank_history({})
+				end,
+				desc = "Clipboard",
+				mode = { "n", "i" },
+			},
+			{
+				"y",
+				"<Plug>(YankyYank)",
+				desc = "Yank text",
+				mode = { "n", "x" },
+			},
+			{
+				"p",
+				"<Plug>(YankyPutAfter)",
+				desc = "Put yanked text after cursor",
+				mode = { "n", "x" },
+			},
+			{
+				"P",
+				"<Plug>(YankyPutBefore)",
+				desc = "Put yanked text before cursor",
+				mode = { "n", "x" },
+			},
+			{ "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
+			{ "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
+		},
+	},
 }
