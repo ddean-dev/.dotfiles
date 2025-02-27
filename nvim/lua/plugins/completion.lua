1c5a889c79ac72a8918fdef1aa155d399047207f
David Dean <david@ddean.dev>
2025-02-26 20:24:34 -0600

    switch to blink

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,71 +1,4 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lsp-signature-help",
-			"L3MON4D3/LuaSnip",
-		},
-		opts = function()
-			vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			local defaults = require("cmp.config.default")()
-			return {
-				completion = {
-					completeopt = "menu,menuone,noinsert",
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({
-						select = true,
-						behavior = cmp.ConfirmBehavior.Replace,
-					}),
-					["<esc>"] = function(fallback)
-						if cmp.visible() then
-							cmp.abort()
-						else
-							fallback()
-						end
-					end,
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.locally_jumpable(1) then
-							luasnip.jump(1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "path" },
-				}),
-				sorting = defaults.sorting,
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-			}
-		end,
-		config = function(_, opts)
-			for _, source in ipairs(opts.sources) do
-				source.group_index = source.group_index or 1
-			end
-			require("cmp").setup(opts)
-		end,
-	},
 	{ "rafamadriz/friendly-snippets" },
 	{
 		"L3MON4D3/LuaSnip",
@@ -75,9 +8,6 @@ return {
 			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 	},
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-cmdline",
 	{
 		"danymat/neogen",
 		config = function()
@@ -93,5 +23,32 @@ return {
 				desc = "Annotate",
 			},
 		},
+	},
+	{
+		"saghen/blink.cmp",
+		dependencies = "rafamadriz/friendly-snippets",
+		version = "*",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "enter" },
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				accept = { auto_brackets = { enabled = false } },
+				menu = { auto_show = false },
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+				trigger = { show_on_trigger_character = true },
+			},
+			sources = {
+				--default = { "lsp", "path", "snippets", "buffer" },
+				default = { "lsp", "path", "snippets" },
+			},
+			snippets = { preset = "luasnip" },
+			signature = { enabled = true },
+		},
+		opts_extend = { "sources.default" },
 	},
 }
