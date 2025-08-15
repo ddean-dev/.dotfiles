964942d28cc14c10392ed7f4aa09366d749a5674
David Dean <david@ddean.dev>
2025-08-15 05:43:14 -0600

    swap clipcat for cliphist

--- install.sh
+++ install.sh
@@ -3,46 +3,30 @@
 echo "#######################"
 echo "# Installing Packages #"
 echo "#######################"
-if [ -x "$(command -v apt)" ]; then
-  sudo apt update && sudo apt upgrade
-  sudo apt install \
-    git ssh keychain \
-    tmux zsh zsh-syntax-highlighting \
-    eza bat ripgrep fzf duf \
-    nodejs npm lua5.4 luarocks golang rust-all
-  if [ ! -x "$(command -v brew)" ] && [ ! -x "$(command -v pacman)" ]; then
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  fi
-  brew install \
-    starship neovim lazygit lazydocker \
-    bottom pamburus/tap/hl
-  npm install -g @bitwarden/cli pnpm
-elif [ -x "$(command -v pacman)" ]; then
-  sudo pacman -Syu
-  sudo pacman -Sy --needed \
-    git keychain man-pages man-db base-devel wl-clipboard \
-    tmux zsh zsh-syntax-highlighting starship clipcat \
-    eza bat ripgrep fzf duf bottom hl jq \
-    cmake nodejs npm luarocks go zig rust \
-    neovim lazygit docker docker-compose docker-buildx bitwarden \
-    typos-lsp lua-language-server bash-language-server gopls pyright ruff \
-    shfmt stylua
-  if [ ! -x "$(command -v yay)" ]; then
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    cd ..
-    rm -rf yay
-  fi
-  yay -Syu --answerclean=NotInstalled --answerdiff=None
-  yay -Sy --needed --answerclean=None --answerdiff=None \
-    lazydocker golangci-lint docker-ls snapd
-  sudo npm i -g typescript eslint eslint_d prettier typescript-language-server vscode-langservers-extracted
-  go install github.com/go-delve/delve/cmd/dlv@latest
-  go install github.com/nametake/golangci-lint-langserver@latest
-  go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
+sudo pacman -Syu
+sudo pacman -Sy --needed \
+  git keychain man-pages man-db base-devel wl-clipboard cliphist \
+  tmux zsh zsh-syntax-highlighting starship \
+  eza bat ripgrep fzf duf bottom hl jq \
+  cmake nodejs npm luarocks go zig rust \
+  elixir erlang erlang-asn1 erlang-public_key erlang-ssl erlang-parsetools erlang-xmerl inotify-tools \
+  neovim lazygit docker docker-compose docker-buildx bitwarden \
+  marksman typos-lsp lua-language-server bash-language-server gopls pyright ruff \
+  shfmt stylua
+if [ ! -x "$(command -v yay)" ]; then
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+  cd ..
+  rm -rf yay
 fi
+yay -Syu --answerclean=NotInstalled --answerdiff=None
+yay -Sy --needed --answerclean=None --answerdiff=None \
+  lazydocker golangci-lint docker-ls snapd elixir-ls
+sudo npm i -g typescript eslint eslint_d prettier typescript-language-server vscode-langservers-extracted
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
 
 echo "#####################"
 echo "# Updating Dotfiles #"
@@ -71,7 +55,7 @@ mkdir -p "$HOME/.config/"
 ln -sfTn "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
 ln -sfTn "$HOME/.dotfiles/lazygit" "$HOME/.config/lazygit"
 ln -sfTn "$HOME/.dotfiles/hl" "$HOME/.config/hl"
-ln -sfTn "$HOME/.dotfiles/clipcat" "$HOME/.config/clipcat"
+ln -sfTn "$HOME/.dotfiles/cliphist" "$HOME/.config/cliphist"
 ln -sfT "$HOME/.dotfiles/starship.toml" "$HOME/.config/starship.toml"
 chmod +x "$HOME/.dotfiles/fzf-tab-format.sh"
 
@@ -79,8 +63,8 @@ echo "#####################"
 echo "# Enabling Services #"
 echo "#####################"
 systemctl --user daemon-reload
-systemctl --user enable "$HOME/.config/clipcat/clipcat.service"
-systemctl --user start clipcat.service
+systemctl --user enable "$HOME/.config/cliphist/cliphist.service"
+systemctl --user start cliphist.service
 
 if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
   echo "######################"
