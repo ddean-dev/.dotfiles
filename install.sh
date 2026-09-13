96846a009482fa20bb1c7ed23358034660edeef8
David Dean <david@ddean.dev>
2026-09-13 07:20:30 -0600

    remove reliance on aur/yay

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 echo "#######################"
 echo "# Installing Packages #"
@@ -9,20 +10,13 @@ sudo pacman -Sy --needed \
   tmux zsh zsh-syntax-highlighting starship \
   eza bat ripgrep fzf duf bottom hl jq \
   cmake nodejs npm pnpm luarocks go zig rust \
-  neovim lazygit docker docker-compose docker-buildx bitwarden \
-  marksman typos-lsp lua-language-server bash-language-server gopls pyright ruff \
+  neovim lazygit docker docker-compose docker-buildx lazydocker \
+  marksman typos-lsp lua-language-server bash-language-server gopls golangci-lint pyright ruff \
   shfmt stylua
-if [ ! -x "$(command -v yay)" ]; then
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si
-  cd ..
-  rm -rf yay
-fi
-yay -Syu --answerclean=NotInstalled --answerdiff=None --answeredit=None
-yay -Sy --needed --answerclean=None --answerdiff=None --answeredit=None \
-  lazydocker golangci-lint docker-ls
-sudo npm i -g typescript eslint eslint_d prettier typescript-language-server vscode-langservers-extracted
+pnpm i -g \
+  typescript eslint eslint_d prettier \
+  typescript-language-server vscode-langservers-extracted \
+  @bitwarden/cli
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
@@ -73,13 +67,12 @@ if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
   ssh-keygen -t ed25519 -C "david@ddean.dev"
   eval $(keychain --eval --quiet)
   ssh-add ~/.ssh/id_ed25519
+  # Display ssh key
+  echo "###########################"
+  echo "# Add below key to GitHub #"
+  echo "###########################"
+  cat ~/.ssh/id_ed25519.pub
+  echo "###########################"
+  echo "# Add above key to GitHub #"
+  echo "###########################"
 fi
-
-# Display ssh key
-echo "###########################"
-echo "# Add below key to GitHub #"
-echo "###########################"
-cat ~/.ssh/id_ed25519.pub
-echo "###########################"
-echo "# Add above key to GitHub #"
-echo "###########################"
