7410613c97d5e01ae4bb3d087846a42ed505291d
David Dean <david@ddean.dev>
2026-09-14 23:14:46 -0600

    add treewalker

--- nvim/lua/plugins/parser.lua
+++ nvim/lua/plugins/parser.lua
@@ -1,6 +1,20 @@
 return {
-	"https://github.com/arborist-ts/arborist.nvim",
-	init = function()
-		require("arborist").setup()
-	end,
+	{
+		"https://github.com/arborist-ts/arborist.nvim",
+		init = function()
+			require("arborist").setup()
+		end,
+	},
+	{
+		"aaronik/treewalker.nvim",
+		opts = {
+			highlight = true, -- Whether to briefly highlight the node after jumping to it
+		},
+		keys = {
+			{ "<M-Up>", "<cmd>Treewalker Up<cr>", { noremap = true, silent = true }, mode = { "n", "v", "i" } },
+			{ "<M-Down>", "<cmd>Treewalker Down<cr>", { noremap = true, silent = true }, mode = { "n", "v", "i" } },
+			{ "<M-Right>", "<cmd>Treewalker Right<cr>", { noremap = true, silent = true }, mode = { "n", "v", "i" } },
+			{ "<M-Left>", "<cmd>Treewalker Left<cr>", { noremap = true, silent = true }, mode = { "n", "v", "i" } },
+		},
+	},
 }
