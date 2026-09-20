871314ab66ba0199d8bdd165bdad543754d75ed9
David Dean <david@ddean.dev>
2026-09-20 10:07:59 -0600

    add wowlua-ls

--- nvim/lua/plugins/language_server_protocol.lua
+++ nvim/lua/plugins/language_server_protocol.lua
@@ -22,6 +22,7 @@ return {
 
 				"bashls", --bash
 				"lua_ls", --lua
+				"wowlua_ls", --wowlua
 
 				"eslint", --javascript, typescript
 				"ts_ls", --typescript
@@ -36,9 +37,38 @@ return {
 				"docker_compose_language_service", --docker
 				"dockerls", --docker
 			}
+
+			--setup custom provider wowlua_ls
+			vim.lsp.config("wowlua_ls", {
+				cmd = { "wowlua_ls" },
+				filetypes = { "lua" },
+				root_markers = { ".wowluarc.json" },
+				root_dir = function(bufnr, on_dir)
+					local root_markers = { ".wowluarc.json" }
+					local cwd = vim.fs.root(bufnr, root_markers)
+					if cwd then
+						on_dir(cwd)
+					end
+				end,
+				workspace_required = false,
+			})
+
+			--disable lua_ls if wowlua_ls active
+			vim.lsp.config("lua_ls", {
+				root_dir = function(bufnr, on_dir)
+					if vim.fs.root(bufnr, { ".wowluarc.json" }) then
+						return
+					end
+					on_dir(vim.fs.root(bufnr, { ".luarc.json" }) or vim.fn.getcwd())
+				end,
+			})
+
+			--enable servers
 			for _, server_name in pairs(useDefaults) do
 				vim.lsp.enable(server_name)
 			end
+
+			--config diagnostics
 			vim.diagnostic.config({
 				virtual_lines = {
 					current_line = true,
